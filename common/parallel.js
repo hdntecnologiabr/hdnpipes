2c77e45ab0717f3208a0039332c36689a117cbb3
Dimi Silva <jnn.sla@gmail.com>
2020-11-17 12:41:10 -0300

    parallel pipe accepts ctx in functions param

--- common/parallel.js
+++ common/parallel.js
@@ -1,12 +1,13 @@
 module.exports = ({
-  functions = [],
+  functions = ctx => [],
   success = (result, ctx) => ({ result, ctx }),
   fail = (err, ctx) => {
     throw err
   }
 }) => async ctx => {
   try {
-    const result = await Promise.all(functions.map(async fn => await fn(ctx)))
+    const _functions = await functions(ctx)
+    const result = await Promise.all(_functions.map(async fn => await fn(ctx)))
     return await success(result, ctx)
   } catch (err) {
     return fail(err, ctx)
