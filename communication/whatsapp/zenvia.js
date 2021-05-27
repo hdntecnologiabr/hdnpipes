ad7bb8f2a852393df5b01d60fe3718a63b26b051
Dimi Silva <jnn.sla@gmail.com>
2021-05-27 19:04:06 -0300

    adjust in Zenvia sendMessage token

--- communication/whatsapp/zenvia.js
+++ communication/whatsapp/zenvia.js
@@ -14,36 +14,37 @@ const defaultFailFn = (err, ctx) => {
   throw err
 }
 
-module.exports.sendMessage = ({
-  token = defaultTokenFn,
-  sender = defaultSenderFn,
-  receiver = defaultReceiverFn,
-  messages = defaultMessagesFn,
-  success = defaultSuccessFn,
-  fail = defaultFailFn
-}) => async ctx => {
-  try {
-    const _token = await token(ctx)
-    const _sender = await sender(ctx)
-    const _receiver = await receiver(ctx)
-    const _messages = await messages(ctx)
+module.exports.sendMessage =
+  ({
+    sender = defaultSenderFn,
+    receiver = defaultReceiverFn,
+    messages = defaultMessagesFn,
+    success = defaultSuccessFn,
+    fail = defaultFailFn
+  }) =>
+    async ctx => {
+      try {
+        const _token = process.env.ZENVIA_TOKEN
+        const _sender = await sender(ctx)
+        const _receiver = await receiver(ctx)
+        const _messages = await messages(ctx)
 
-    const result = await axios({
-      method: 'POST',
-      url: 'https://api.zenvia.com/v2/channels/whatsapp/messages',
-      headers: {
-        'X-API-TOKEN': _token,
-        'Content-Type': 'application/json'
-      },
-      data: {
-        from: _sender,
-        to: _receiver,
-        contents: _messages
+        const result = await axios({
+          method: 'POST',
+          url: 'https://api.zenvia.com/v2/channels/whatsapp/messages',
+          headers: {
+            'X-API-TOKEN': _token,
+            'Content-Type': 'application/json'
+          },
+          data: {
+            from: _sender,
+            to: _receiver,
+            contents: _messages
+          }
+        })
+
+        return success(result, ctx)
+      } catch (err) {
+        return fail(err, ctx)
       }
-    })
-
-    return success(result, ctx)
-  } catch (err) {
-    return fail(err, ctx)
-  }
-}
+    }
